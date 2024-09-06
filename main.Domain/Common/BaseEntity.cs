7a1dd759bc0b2c096374b356b81d70c5f2628ce1
Vladimir <mrdontofgaming@gmail.com>
2024-09-06 21:55:00 +0300

    Добавленны базовые сущности

---
+++ main.Domain/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main.domain.Common
+{
+    internal class BaseEntity : IEntityId, IEntiteDate
+    {
+        public long id { get; set; }
+        public DateTime DateCreate { get ; set; }
+        public DateTime DateUpdate { get ; set; }
+    }
+}
---
+++ main.Domain/Common/IEntiteDate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main.domain.Common
+{
+    public interface IEntiteDate
+    {
+        public DateTime DateCreate { get; set; }
+        public DateTime DateUpdate { get; set; }
+    }
+}
