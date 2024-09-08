a09d5d0a033038dcb2f4e695b672d1fc9d7ee457
Vladimir <mrdontofgaming@gmail.com>
2024-09-08 16:35:45 +0300

    Добавлена сущность сотрудника и должности

---
+++ main.Domain/Models/Employee/Employee.cs
@@ -0,0 +1,56 @@
+﻿using main.domain.Common;
+using System;
+
+namespace main.domain.Models.Employee;
+
+/// <summary>
+/// Класс сотрудника
+/// </summary>
+public class Employee : BaseEntity
+{
+    private Employee(string name, long companyId, long roleId)
+    {
+        Name = name;
+        CompanyId = companyId;
+        RoleId = roleId;
+    }
+
+    /// <summary>
+    /// ФИО сотрудника
+    /// </summary>
+    public string Name { get; private set; }
+
+    /// <summary>
+    /// Индетификатор компании, в которой работает сотрудник
+    /// </summary>
+    public long CompanyId { get; private set; }
+
+    /// <summary>
+    /// Идентификатор долности сотрудника
+    /// </summary>
+    public long RoleId { get; private set; }
+
+    /// <summary>
+    /// Метод создания сотрудника
+    /// </summary>
+    /// <param name="name">ФИО сотрудника</param>
+    /// <param name="companyId">Идентификатор компании</param>
+    /// <param name="roleId">Идентификатор роли</param>
+    /// <returns>Сущность сотрудника</returns>
+    public static Employee? Create(string name, long companyId, long roleId)
+    {
+        if (String.IsNullOrEmpty(name))
+        {
+            return null;
+        }
+
+        if (companyId <= 0 || roleId <= 0)
+        {
+            return null;
+        }
+
+        var employee = new Employee(name, companyId, roleId);
+
+        return employee;
+    }
+}
---
+++ main.Domain/Models/Employee/Role.cs
@@ -0,0 +1,66 @@
+﻿using main.domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main.domain.Models.Employee
+{
+    /// <summary>
+    /// Класс должности сотрудника в компании
+    /// </summary>
+    public class Role : BaseEntity
+    {
+        /// <summary>
+        /// Минимально допутимая длина наименования роли
+        /// </summary>
+        private const int MinLengthName = 3;
+
+        private Role(string name, long companyId)
+        {
+            Name = name;
+            CompanyId = companyId;
+        }
+
+        /// <summary>
+        /// Наименование должность
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Идентификатор компании, в которой принадлежит должность
+        /// </summary>
+        public long CompanyId { get; private set; }
+
+        /// <summary>
+        /// Список сотрудников, которым принадлежит должность
+        /// </summary>
+        List<Employee> Employees { get; set; } = new();
+
+        /// <summary>
+        /// Создание новой должностит
+        /// </summary>
+        /// <param name="name"> Наименование должности</param>
+        /// <param name="companyId"> Идентификатор компании</param>
+        /// <returns>Возвращает сущность должноти</returns>
+        public static Role? Create(string name, long companyId)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                return null;
+            }
+
+            if (name.Length < MinLengthName)
+            {
+                return null;
+            }
+
+            var role = new Role(name, companyId);
+
+            return role;
+        }
+
+    }
+}
