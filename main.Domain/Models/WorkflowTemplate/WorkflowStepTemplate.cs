c85a9d6082db549a8b93204ac9e0427719c93ec9
Vladimir <mrdontofgaming@gmail.com>
2024-09-09 19:31:34 +0300

    Созданы и описаны сущности Шаблона workflow и шаблона шага

---
+++ main.Domain/Models/WorkflowTemplate/WorkflowStepTemplate.cs
@@ -0,0 +1,123 @@
+﻿using main.domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace main.domain.Models.WorkflowTemplate
+{
+    /// <summary>
+    /// Шаг в шаблоне Workflow
+    /// </summary>
+    public class WorkflowStepTemplate
+    {
+        private WorkflowStepTemplate(int number, string description, long? eployerId, long? roleId, WorkflowTemplate workflowTemplate)
+        {
+            Number = number;
+            Description = description;
+            EployerId = eployerId;
+            RoleId = roleId;
+            WorkflowTemplate = workflowTemplate;
+            WorkflowTemplateId = workflowTemplate.Id;
+        }
+
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string Description { get; private set; }
+
+        /// <summary>
+        /// Порядковый номер
+        /// </summary>
+        public int Number { get; private set; }
+
+        /// <summary>
+        /// Идентификатор сотрудника, который должен будет исполнять шаг
+        /// </summary>
+        public long? EployerId { get; private set; }
+
+        /// <summary>
+        /// Идентификатор должности, которая должна будет исполнять шаг
+        /// </summary>
+        public long? RoleId { get; private set; }
+
+        /// <summary>
+        /// Сущность шаблона
+        /// </summary>
+        public WorkflowTemplate WorkflowTemplate { get; private set; }
+
+        public long WorkflowTemplateId { get; private set; }
+
+        /// <summary>
+        /// Создание нового шага
+        /// </summary>
+        /// <param name="number">Порядковый номер</param>
+        /// <param name="description">Описание</param>
+        /// <param name="eployerId">Идентификатор сотрудника</param>
+        /// <param name="roleId">Идентификатор роли</param>
+        /// <param name="workflowTemplate">Сущность шаблона</param>
+        /// <returns></returns>
+        internal static Result<WorkflowStepTemplate> Create(int number, string description, long? eployerId, long? roleId, WorkflowTemplate workflowTemplate)
+        {
+            if (number <= 0)
+            {
+                return Result<WorkflowStepTemplate>.Failure($"{number} - некорректное значение для номера шага");
+            }
+
+            if (eployerId is null && roleId is null)
+            {
+                return Result<WorkflowStepTemplate>.Failure($"{eployerId} or {roleId}: У шага должна быть привязка к конкретногому сотруднику или должности");
+            }
+
+            if (eployerId is not null && eployerId <= 0)
+            {
+                return Result<WorkflowStepTemplate>.Failure($"{eployerId} - некорректное значение для идентификатора сотрудника в шаге");
+            }
+
+            if (roleId is not null && eployerId <= 0)
+            {
+                return Result<WorkflowStepTemplate>.Failure($"{roleId} - некорректное значение для идентификатора должности в шаге");
+            }
+
+            if (workflowTemplate is null)
+            {
+                return Result<WorkflowStepTemplate>.Failure($"{nameof(workflowTemplate)} не может быть null");
+            }
+
+            var stepTemplate = new WorkflowStepTemplate(number, description, eployerId, roleId, workflowTemplate);
+
+            return Result<WorkflowStepTemplate>.Success(stepTemplate);  
+        }
+
+        /// <summary>
+        /// Обновление данных шага
+        /// </summary>
+        /// <param name="description">Описание</param>
+        /// <returns></returns>
+        public Result<bool> UpdateInfo(string description)
+        {
+
+            Description = description.Trim();
+            
+            return Result<bool>.Success(true);
+        }
+
+        public Result<bool> UpdateNumber(int number)
+        {
+            if (number <= 0)
+            {
+                return Result<bool>.Failure($"{number} - некорректное значение для номера шага");
+            }
+
+            Number = number;
+
+            return Result<bool>.Success(true);
+        }
+
+
+
+    }
+}
---
+++ main.Domain/Models/WorkflowTemplate/WorkflowTemplate.cs
@@ -0,0 +1,190 @@
+﻿using main.domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main.domain.Models.WorkflowTemplate
+{
+    /// <summary>
+    /// Шаблон Workfloy
+    /// </summary>
+    public class WorkflowTemplate : BaseEntity
+    {
+        /// <summary>
+        /// Константное значение минимальной длины наименования шаблона
+        /// </summary>
+        public const int MinLengthName = 5;
+
+        /// <summary>
+        /// Защищенный список шагов
+        /// </summary>
+        private readonly List<WorkflowStepTemplate> _steps = new();
+
+        private WorkflowTemplate(string name, string description, long companyId)
+        {
+            Name = name;
+            Description = description;
+            CompanyId = companyId;
+        }
+
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Описание 
+        /// </summary>
+        public string Description { get; private set; }
+
+        /// <summary>
+        /// Идентификатор компании, которой принадлежит шаблон
+        /// </summary>
+        public long CompanyId { get; }
+
+        /// <summary>
+        /// Безопасный список, для чтения из вне
+        /// </summary>
+        public IReadOnlyCollection<WorkflowStepTemplate> Steps => _steps;
+
+        /// <summary>
+        /// Метод создание нового шаблона
+        /// </summary>
+        /// <param name="name">Наименование</param>
+        /// <param name="description">Описание</param>
+        /// <param name="companyId">Идентификатор компании</param>
+        /// <returns>Сущность с результатом создания</returns>
+        public static Result<WorkflowTemplate> Create(string name, string description, long companyId)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                return Result<WorkflowTemplate>.Failure("Наименование шаблона не может быть пустым");
+            }
+
+            if (name.Trim().Length < MinLengthName)
+            {
+                return Result<WorkflowTemplate>.Failure($"Длина наименование шаблона не может быть меньше {MinLengthName}");
+            }
+
+            if (companyId <= 0)
+            {
+                return Result<WorkflowTemplate>.Failure($"{companyId} - некорректный идентификатор компании");
+            }
+
+            var workflowTemplate = new WorkflowTemplate(name.Trim(), description, companyId);
+
+            return Result<WorkflowTemplate>.Success(workflowTemplate);
+        }
+
+        /// <summary>
+        /// Метод обновления информации
+        /// </summary>
+        /// <param name="name">Наименование</param>
+        /// <param name="description">Описание</param>
+        /// <returns>Результат обновления информации</returns>
+        public Result<bool> UpdateInfo(string? name, string? description)
+        {
+            if (name is not null)
+            {
+                if (String.IsNullOrEmpty(name))
+                {
+                    return Result<bool>.Failure("Наименование шаблона не может быть пустым");
+                }
+
+                if (name.Trim().Length < MinLengthName)
+                {
+                    return Result<bool>.Failure($"Длина наименование шаблона не может быть меньше {MinLengthName}");
+                }
+
+                Name = name.Trim();
+            }
+
+            if (description is not null)
+            {
+                Description = description.Trim();
+            }
+
+            return Result<bool>.Success(true);
+        }
+
+        /// <summary>
+        /// Добавление нового шага
+        /// </summary>
+        /// <param name="description">Описание</param>
+        /// <param name="employerId">Идентификатор сотрудника, исполняемого шаг</param>
+        /// <param name="roleId">Идентификатор должности, исполняемой шаг</param>
+        /// <returns>Результат добавления шага</returns>
+        public Result<bool> AddStep(string description, long? employerId, long? roleId)
+        {
+            var createStep = WorkflowStepTemplate.Create(_steps.Count + 1, description, employerId, roleId, this);
+
+            if (createStep.IsFailure)
+            {
+                return Result<bool>.Failure($"Добавление элемента в список провалиловсь: {createStep.FailureMessage}");
+            }
+
+            var step = createStep.Data;
+
+            _steps.Add(step);
+
+            return Result<bool>.Success(true);
+        }
+
+        /// <summary>
+        /// Удаление шага
+        /// </summary>
+        /// <param name="number">Номер шага</param>
+        /// <returns>Успешность удаления</returns>
+        public Result<bool> RemoveStep(int number)
+        {
+            if (number > _steps.Count)
+            {
+                return Result<bool>.Failure($"Шаблон не содержит шаг с таким номером");
+            }
+
+            _steps.RemoveAt(number-1);
+            UpdateStepNumbers(number);
+
+            return Result<bool>.Success(true);
+        }
+
+        /// <summary>
+        /// Обмен номерами у шагов
+        /// </summary>
+        /// <param name="numberFirst">Номер первого шага</param>
+        /// <param name="numberSecond">Номер второго шага</param>
+        /// <returns>Успешность обмена</returns>
+        public Result<bool> SwapSteps(int numberFirst, int numberSecond)
+        {
+            if (_steps.Count < numberFirst || _steps.Count < numberSecond) 
+            {
+                return Result<bool>.Failure($"Шаблон не содержит шаг с таким номером");
+            }
+
+            _steps[numberFirst - 1].UpdateNumber(numberSecond);
+            _steps[numberSecond - 1].UpdateNumber(numberFirst);
+
+            return Result<bool>.Success(true);
+        }
+
+        //Вынес в отдельный метод, т.к. может пригодиться в нескольких местах класса, а может и нет =)
+        /// <summary>
+        /// Проходит по писку от {number} и обновляет значение номера шага
+        /// </summary>
+        /// <param name="number">Номер шага, который был удален</param>
+        private void UpdateStepNumbers(int number)
+        {
+            if (number <= _steps.Count)
+            {
+                for (int i = number - 1; i < _steps.Count; i++)
+                {
+                    _steps[i].UpdateNumber(i + 1);
+                }
+            }
+        }
+    }
+}
