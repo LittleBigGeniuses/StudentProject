575b94335a7b5fa037abd157dfb0e5801b5f681b
Vladimir <mrdontofgaming@gmail.com>
2024-09-19 19:12:57 +0300

    Добавлено дефолтное значение в статус

--- main.Domain/Workflow/Enum/Status.cs
+++ main.Domain/Workflow/Enum/Status.cs
@@ -5,19 +5,23 @@
 /// </summary>
 public enum Status
 {
+    /// <summary>
+    /// Пустное значение, возникающее в случае ошибки
+    /// </summary>
+    Default = 0,
 
     /// <summary>
     /// Ожидание
     /// </summary>
-    Expectation = 0,
+    Expectation = 1,
 
     /// <summary>
     /// Одобренно
     /// </summary>
-    Approved = 1,
+    Approved = 2,
 
     /// <summary>
     /// Отказано
     /// </summary>
-    Rejected = 2
+    Rejected = 3
 }
