928255bdc098ccbbf1719aa8bf004117a129c334
Vladimir <mrdontofgaming@gmail.com>
2024-10-17 03:57:34 +0300

    Добавлено расширение для подключение кастомизаций

---
+++ main.DomainTest/Customizations/FixtureCustomizationExtension.cs
@@ -0,0 +1,24 @@
+﻿using AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main.DomainTest.Customizations
+{
+    /// <summary>
+    /// Расширение для подключения кастомизаций
+    /// </summary>
+    public static class FixtureCustomizationExtension
+    {
+        public static void FixtureCustomization(this IFixture fixture)
+        {
+            fixture.Customize(new WorkflowCustomization());
+            fixture.Customize(new EmployeeCustomization());
+            fixture.Customize(new WorkflowTemplateWithStepsCustomization());
+        }
+    }
+}
