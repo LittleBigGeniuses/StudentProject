239519b3858f098696b4297742185375ee5dd535
Vladimir <mrdontofgaming@gmail.com>
2024-10-17 03:45:15 +0300

    Создан кастомный кастомизатор для создания воркфлоу

---
+++ main.DomainTest/Customizations/WorkflowCustomization.cs
@@ -0,0 +1,25 @@
+﻿using Main.Domain.WorkflowDomain;
+using Main.Domain.WorkflowTemplateDomain;
+
+namespace main.DomainTest.Customizations
+{
+    public class WorkflowCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<Workflow>(composer =>
+                composer.FromFactory(() =>
+                {
+                    fixture.Customize(new WorkflowTemplateWithStepsCustomization());
+                    var validTemplate = fixture.Create<WorkflowTemplate>();
+
+                    var validAuthorId = fixture.Create<Guid>();
+                    var validCandidateId = fixture.Create<Guid>();
+
+                    var workflow = Workflow.Create(validAuthorId, validCandidateId, validTemplate).Value;
+
+                    return workflow!;
+                }));
+        }
+    }
+}
