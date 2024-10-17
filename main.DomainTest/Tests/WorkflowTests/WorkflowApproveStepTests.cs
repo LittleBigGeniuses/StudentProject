4900d768cec2aaabae377658a6e973ade3454400
Vladimir <mrdontofgaming@gmail.com>
2024-10-17 03:38:55 +0300

    Создан класс тестов

---
+++ main.DomainTest/Tests/WorkflowTests/WorkflowApproveStepTests.cs
@@ -0,0 +1,17 @@
+﻿using Main.Domain.EmployeeDomain;
+using Main.Domain.WorkflowDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main.DomainTest.Tests.WorkflowTests
+{
+    public class WorkflowApproveStepTests
+    {
+        private readonly Workflow _workflow;
+        private readonly Employee _employee;
+
+    }
+}
