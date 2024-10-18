f6b7e50df4c1bc2caa5eb700faa24ad980648fb6
Vladimir <mrdontofgaming@gmail.com>
2024-10-18 21:02:29 +0300

    Убрано дублирование

--- main.DomainTest/Models/CompanyCtor.cs
+++ main.DomainTest/Models/CompanyCtor.cs
@@ -11,48 +11,6 @@ namespace main.DomainTest.Models
             DateTime dateCreate, 
             DateTime dateUpdate) 
             : base(id, name, description, dateCreate, dateUpdate)
-        {
-            if (id == Guid.Empty)
-            {
-                throw new ArgumentNullException($"{id} - некорректный идентификатор Компании");
-            }
-
-            if (String.IsNullOrEmpty(name))
-            {
-                throw new ArgumentNullException("Наименование компании не может быть пустым");
-            }
-
-            if (String.IsNullOrEmpty(description))
-            {
-                throw new ArgumentNullException("Описание компании не может быть пустым");
-            }
-
-            if (dateCreate == DateTime.MinValue)
-            {
-                throw new ArgumentException("Дата создания не может быть дефолтной.");
-            }
-
-            if (dateUpdate == DateTime.MinValue)
-            {
-                throw new ArgumentException("Дата обновления не может быть дефолтной.");
-            }
-
-            if (name.Trim().Length < MinLengthName)
-            {
-                throw new ArgumentException($"Длина наименования компании не может быть меньше {MinLengthName}");
-            }
-
-            Id = id;
-            Name = name;
-            Description = description;
-            DateCreate = dateCreate;
-            DateUpdate = dateUpdate;
-        }
-
-        public Guid Id { get; }
-        public string Name { get; private set; }
-        public string Description { get; private set; }
-        public DateTime DateCreate { get; }
-        public DateTime DateUpdate { get; private set; }
+        {}
     }
 }
