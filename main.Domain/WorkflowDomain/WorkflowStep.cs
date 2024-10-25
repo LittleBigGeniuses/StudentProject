de18e6ee8364843522d8614243d0c51fb6e6cf18
Vladimir <mrdontofgaming@gmail.com>
2024-10-25 23:20:15 +0300

    Правки код-стайла

--- main.Domain/WorkflowDomain/WorkflowStep.cs
+++ main.Domain/WorkflowDomain/WorkflowStep.cs
@@ -39,7 +39,7 @@ namespace Main.Domain.WorkflowDomain
                 throw new ArgumentOutOfRangeException("Некорректный номер шага процесса");
             }
 
-            if (String.IsNullOrEmpty(description))
+            if (string.IsNullOrEmpty(description))
             {
                 throw new ArgumentNullException("Описание шага процесса не может быть пустым");
             }
@@ -78,8 +78,8 @@ namespace Main.Domain.WorkflowDomain
             DateUpdate = dateUpdate;
             Status = status;
             DelegatedEmployeeId = delegatedEmployeeId;
-            DelegateStartTime = DelegateStartTime;
-            DelegateEndTime = DelegateEndTime;
+            DelegateStartTime = delegateStartTime;
+            DelegateEndTime = delegateEndTime;
             RestartAuthorEmployeeId = restartAuthorEmployeeId;
             RestartDate = restartDate;
         }
@@ -115,7 +115,11 @@ namespace Main.Domain.WorkflowDomain
                                         DateTime.UtcNow, 
                                         DateTime.UtcNow, 
                                         Status.Expectation, 
-                                        null, null, null, null, null);
+                                        null, 
+                                        null, 
+                                        null, 
+                                        null, 
+                                        null);
 
             return Result<WorkflowStep>.Success(step);
         }
@@ -138,7 +142,7 @@ namespace Main.Domain.WorkflowDomain
         /// <summary>
         /// Отзыв сотрудника по шагу
         /// </summary>
-        public string? Feedback { get; private set; } = null;
+        public string? Feedback { get; private set; } 
 
         /// <summary>
         /// Описание шага
